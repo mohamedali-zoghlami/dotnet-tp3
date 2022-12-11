15e2934632578f493640fb006264113db7153f55
Med Ali Zoghlami <mohamedali.zoghlami@insat.ucar.tn>
2022-12-11 23:47:59 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using System.Data.SQLite;
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=index}/{id?}");
+
+
+app.Run();
